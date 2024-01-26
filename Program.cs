9e7e2569f2af5abfa42eb88fa5b6abc5c5174e1c
David Smith <cpmpdavidsmith@icloud.com>
2024-01-26 15:23:58 -0500

    djjfirfw

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Demo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> animalNames = new List<string>
+            { "fawn", "gibbon", "heron", "ibex", "jackalope"};
+
+            IEnumerable<string> longAnimalNames =
+                animalNames.Where(name => name.Contains("o"));
+
+            foreach (var i in longAnimalNames)
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.Read();
+        }
+    }
+}
